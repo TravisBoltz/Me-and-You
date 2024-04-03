b206aab33cc783068188236de5f9046e3c835ab4
TravisBoltz <fariduoubdamusah@gmail.com>
2024-04-02 17:18:24 -0700

    chatArea Ui

---
+++ src/components/chatArea.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ChatArea = () => {
+    return (
+        <section className="flex flex-1 dark:bg-gray-600">Chat area</section>
+    );
+}
+
+
+export default ChatArea;
---
+++ src/components/submit.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Submit = () => {
+    return (
+        <main className='dark:bg-gray-600'>
+            <input className='rounded-md h-10 w-96 mx-auto flex mb-2 border-black border-2 dark:border-gray-600' aria-placeholder='Type Your Message....' type='text'></input>
+        </main>
+    );
+}
+
+
+export default Submit;
